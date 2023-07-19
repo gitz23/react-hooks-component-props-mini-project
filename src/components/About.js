6164cf8d8d5d9819fa6bd889ce48fd637c60fff0
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-19 23:11:32 +0300

    done with about

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About(props){
+    const defaultImage = "https://via.placeholder.com/215"
+    const realImage = props.image
+
+    return(
+        <aside>
+            {realImage ? ( <img src={realImage}  alt="blog logo"/>) : (<img src={defaultImage} alt="blog logo" />) }
+            <p>{props.about}</p>
+        </aside>
+    )
+};
+
+export default About;
